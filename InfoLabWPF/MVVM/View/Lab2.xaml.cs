b383dc8886ed0437379540f4e8b55e05747076a4
Fllugel <135070466+Fllugel@users.noreply.github.com>
2024-09-30 18:40:27 +0300

    typo fix to previous commit

--- InfoLabWPF/MVVM/View/Lab2.xaml.cs
+++ InfoLabWPF/MVVM/View/Lab2.xaml.cs
@@ -14,25 +14,17 @@ namespace InfoLabWPF.MVVM.View
         }
     }
 
-    using System;
-    using System.Globalization;
-    using System.Windows;
-    using System.Windows.Data;
-
-    namespace InfoLabWPF.MVVM.View
+    public class StringToVisibilityConverter : IValueConverter
     {
-        public class StringToVisibilityConverter : IValueConverter
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                // Convert the string value to Visibility
-                return string.IsNullOrWhiteSpace(value as string) ? Visibility.Visible : Visibility.Collapsed;
-            }
+            // Convert the string value to Visibility
+            return string.IsNullOrWhiteSpace(value as string) ? Visibility.Visible : Visibility.Collapsed;
+        }
 
-            public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                return (value is Visibility visibility && visibility == Visibility.Visible) ? string.Empty : null;
-            }
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is Visibility visibility && visibility == Visibility.Visible) ? string.Empty : null;
         }
     }
 }
