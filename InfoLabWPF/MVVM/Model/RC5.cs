7b998c1e6316502a1475a172d689353abb940a4c
Fllugel <135070466+Fllugel@users.noreply.github.com>
2024-10-10 22:33:57 +0300

    Lab3 innit

---
+++ InfoLabWPF/MVVM/Model/RC5.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace InfoLabWPF.MVVM.Model
+{
+    public class RC5
+    {
+        private readonly byte[] _key;
+
+        public RC5(string passwordPhrase)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                _key = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passwordPhrase));
+            }
+        }
+
+        public void EncryptStream(Stream inputStream, Stream outputStream, Action<double> progressCallback)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = _key;
+                aes.GenerateIV(); 
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                outputStream.Write(aes.IV, 0, aes.IV.Length);
+
+                using (var cryptoStream = new CryptoStream(outputStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    long totalBytesRead = 0;
+                    long inputLength = inputStream.Length;
+
+                    while ((bytesRead = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        cryptoStream.Write(buffer, 0, bytesRead);
+                        totalBytesRead += bytesRead;
+                        progressCallback((double)totalBytesRead / inputLength * 100); 
+                    }
+                }
+            }
+        }
+
+        public void DecryptStream(Stream inputStream, Stream outputStream)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = _key;
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                byte[] iv = new byte[16];
+                inputStream.Read(iv, 0, iv.Length);
+                aes.IV = iv;
+
+                using (var cryptoStream = new CryptoStream(inputStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+
+                    while ((bytesRead = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        outputStream.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+    }
+}
--- InfoLabWPF/MVVM/ViewModel/Lab3ViewModel.cs
+++ InfoLabWPF/MVVM/ViewModel/Lab3ViewModel.cs
@@ -1,6 +1,155 @@
-﻿namespace InfoLabWPF.MVVM.ViewModel;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using System.Collections;
+using CommunityToolkit.Mvvm.Input;
+using InfoLabWPF.MVVM.Model;
 
-public class Lab3ViewModel
+namespace InfoLabWPF.MVVM.ViewModel
 {
-    
+    public class Lab3ViewModel : INotifyPropertyChanged
+    {
+        private BitArray _passwordPhrase; 
+        private string _selectedFileName;
+        private string _selectDecryptFileName;
+        private double _encryptionProgress;
+        private double _decryptProgress;
+
+        private RC5 _rc5;
+        private MD5 _md5;
+
+        public Lab3ViewModel()
+        {
+            _md5 = new MD5();
+            EncryptCommand = new RelayCommand(EncryptFile);
+            DecryptCommand = new RelayCommand(DecryptFile);
+            SelectFileCommand = new RelayCommand(SelectFile);
+            SelectDecryptFileCommand = new RelayCommand(SelectDecryptFile);
+        }
+
+        public ICommand EncryptCommand { get; }
+        public ICommand DecryptCommand { get; }
+        public ICommand SelectFileCommand { get; }
+        public ICommand SelectDecryptFileCommand { get; }
+
+        public string PasswordPhrase 
+        {
+            set
+            {
+                _passwordPhrase = GetEncryptionKeyFromPassword(value, 16);
+
+                byte[] passwordBytes = BitArrayToByteArray(_passwordPhrase);
+                string passwordString = Encoding.UTF8.GetString(passwordBytes);
+
+                Console.WriteLine(BitArrayToString(_passwordPhrase)); 
+                _rc5 = new RC5(passwordString); 
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedFileName
+        {
+            get => _selectedFileName;
+            set
+            {
+                _selectedFileName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectDecryptFileName
+        {
+            get => _selectDecryptFileName;
+            set
+            {
+                _selectDecryptFileName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double EncryptionProgress
+        {
+            get => _encryptionProgress;
+            set
+            {
+                _encryptionProgress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double DecryptProgress
+        {
+            get => _decryptProgress;
+            set
+            {
+                _decryptProgress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void EncryptFile()
+        {
+            // Implement file encryption logic
+        }
+
+        private void DecryptFile()
+        {
+            // Implement file decryption logic
+        }
+
+        private void SelectFile()
+        {
+            // Implement file selection for encryption
+        }
+
+        private void SelectDecryptFile()
+        {
+            // Implement file selection for decryption
+        }
+
+        private BitArray GetEncryptionKeyFromPassword(string password, int bitsCount)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] hashBytes = _md5.ComputeHash(passwordBytes);
+
+            ulong hashAsInt = BitConverter.ToUInt64(hashBytes, 0);
+
+            // Convert hashAsInt to a bit array of the desired length (bitsCount)
+            BitArray encryptionKey = new BitArray(bitsCount);
+            for (int i = 0; i < bitsCount; i++)
+            {
+                encryptionKey[i] = (hashAsInt & (1UL << (bitsCount - 1 - i))) != 0;
+            }
+
+            return encryptionKey;
+        }
+
+        private byte[] BitArrayToByteArray(BitArray bitArray)
+        {
+            int numBytes = (bitArray.Length + 7) / 8; 
+            byte[] bytes = new byte[numBytes];
+            bitArray.CopyTo(bytes, 0); 
+            return bytes;
+        }
+
+        private string BitArrayToString(BitArray bitArray)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (bool bit in bitArray)
+            {
+                sb.Append(bit ? "1" : "0");
+            }
+            return sb.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
